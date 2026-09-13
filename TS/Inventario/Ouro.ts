fe9d7a58eaffbe7370eb09c1c90c61e5f48fff33
juliana <senactds@gmail.com>
2026-09-13 16:31:39 -0300

    inventário

--- TS/Inventario/Ouro.ts
+++ TS/Inventario/Ouro.ts
@@ -6,8 +6,7 @@ export class Ouro implements Item {
 
     private nome: string = 'Ouro';
     private tipo: Tipo = Tipo.OURO; // Tipo indice
-    private qtd: number = 0
-
+    private qtd: number = 0;
 
     getNome(): string {
         return this.nome;
